bd96207058057b653a3395843fd49736dd69f8a9
Shashank SH <shashankssh07@gmail.com>
2025-05-23 21:29:24 +0530

    s

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Portfolio/', // <-- base should be here, not inside plugins
+  base: 'sha-portfolio', // <-- base should be here, not inside plugins
   plugins: [react(), tailwindcss()],
 })
