f3d3b7df160af8c310865a42297af6ff01b3ff99
Shashank SH <shashankssh07@gmail.com>
2025-05-23 20:27:59 +0530

    n

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Portfolio/', // <-- base should be here, not inside plugins
   plugins: [react(), tailwindcss()],
-   base: '/Portfolio/'
 })
